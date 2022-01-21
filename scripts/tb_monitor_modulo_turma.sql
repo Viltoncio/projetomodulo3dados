8404c204e8da7631d041a5feb980e0d897825654
Viltoncio <94490579+Viltoncio@users.noreply.github.com>
2022-01-21 16:11:08 -0300

    Update tb_monitor_modulo_turma.sql

--- scripts/tb_monitor_modulo_turma.sql
+++ scripts/tb_monitor_modulo_turma.sql
@@ -1,6 +1,16 @@
 CREATE TABLE IF NOT EXISTS "tb_monitor_modulo_turma" (
-    "ID_modulo_turma" INT,
-    "ID_monitor" INT
+    "ID_modulo_turma" integer,
+    "ID_monitor" integer,
+    CONSTRAINT "ID_modulo_turma" FOREIGN KEY ("ID_modulo_turma")
+        REFERENCES public.tb_modulo_turma ("ID_modulo_turma") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT "ID_monitor" FOREIGN KEY ("ID_monitor")
+        REFERENCES public.tb_monitor ("ID_monitor") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 );
 INSERT INTO "tb_monitor_modulo_turma" VALUES
     (1011,1),
