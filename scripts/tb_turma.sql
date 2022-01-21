b2f04fd3b795c3b0343ee93502c9a771d1fa018c
Viltoncio <94490579+Viltoncio@users.noreply.github.com>
2022-01-21 16:13:29 -0300

    Update tb_turma.sql

--- scripts/tb_turma.sql
+++ scripts/tb_turma.sql
@@ -1,8 +1,13 @@
 CREATE TABLE IF NOT EXISTS "tb_turma" (
-    "ID_turma" INT not null PRIMARY KEY,
-    "ID_curso" INT,
-    "turno" TEXT,
-    "status" TEXT
+    "ID_turma" integer NOT NULL,
+    "ID_curso" integer,
+    turno text COLLATE pg_catalog."default",
+    status text COLLATE pg_catalog."default",
+    CONSTRAINT tb_turma_pkey PRIMARY KEY ("ID_turma"),
+    CONSTRAINT "ID_curso" FOREIGN KEY ("ID_curso")
+        REFERENCES public.tb_curso ("ID_curso") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
 INSERT INTO "tb_turma" VALUES
     (1,1,'manha','em andamento'),
