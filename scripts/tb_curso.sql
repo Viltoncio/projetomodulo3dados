627a384b2393926e7ef6495aff8d500c934283de
Viltoncio <94490579+Viltoncio@users.noreply.github.com>
2022-01-21 20:14:53 -0300

    Add files via upload

--- scripts/tb_curso.sql
+++ scripts/tb_curso.sql
@@ -1,9 +1,17 @@
-CREATE TABLE IF NOT EXISTS "tb_curso" (
-    "ID_curso" INT not null PRIMARY KEY,
-    "nome" TEXT,
-    "assuntos" TEXT,
-    "carga_horaria" INT
-);
+CREATE TABLE IF NOT EXISTS public.tb_curso
+(
+    "ID_curso" integer NOT NULL,
+    nome text COLLATE pg_catalog."default",
+    assuntos text COLLATE pg_catalog."default",
+    carga_horaria integer,
+    CONSTRAINT tb_curso_pkey PRIMARY KEY ("ID_curso")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tb_curso
+    OWNER to postgres;
+	
 INSERT INTO "tb_curso" VALUES
-    (1,'Análise de Dados','Lógica de programação, Python, Banco de dados, Google Colab, Pandas',560),
-    (2,'WebDev Full Stack','JavaScript, Node, SQL, HTML/CSS, Rest API, React ',560);
+    (1.0,'Análise de Dados','Lógica de programação, Python, Banco de dados, Google Colab, Pandas',560.0),
+    (2.0,'WebDev Full Stack','JavaScript, Node, SQL, HTML/CSS, Rest API, React ',560.0);
